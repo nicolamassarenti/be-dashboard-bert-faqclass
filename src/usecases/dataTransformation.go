ead10b2a33d7382b6eda85a8ba3a5627d120f08c
NicolaMassarenti <nicola.massarenti@gmail.com>
2020-07-31 21:27:36 +0200

    Added the usecases and utils functions

---
+++ src/usecases/dataTransformation.go
@@ -0,0 +1,40 @@
+package usecases
+
+import (
+	"github.com/NicolaMassarenti/be-dashboard-bert-faqclass/src/domain"
+)
+
+// faqFromDomainLayer transfroms a FAQ from the struct of domain to the struct of the usecases layer
+func faqFromDomainLayer(faq domain.Faq) Faq {
+	answers := make([]Answer, len(faq.Answers))
+	for i, ans := range faq.Answers {
+		answers[i] = Answer{ans.Lang, ans.Answer}
+	}
+
+	trainingExamples := make([]TrainingExample, len(faq.TrainingExamples))
+	for i, example := range faq.TrainingExamples {
+		trainingExamples[i] = TrainingExample{example.Language, example.Examples}
+	}
+	return Faq{faq.ID, faq.MainExample, answers, faq.IsTrained, trainingExamples}
+}
+
+// faqToDomainLayer transfroms a FAQ from the struct of usecases to the one of the domain layer
+func faqToDomainLayer(faq Faq) domain.Faq {
+
+	answers := make([]domain.Answer, len(faq.Answers))
+	for i, ans := range faq.Answers {
+		answers[i] = domain.Answer{ans.lang, ans.answer}
+	}
+
+	trainingExamples := make([]domain.TrainingExample, len(faq.TrainingExamples))
+	for i, example := range faq.TrainingExamples {
+		trainingExamples[i] = domain.TrainingExample{example.Language, example.Examples}
+	}
+	return domain.Faq{
+		ID:               faq.ID,
+		MainExample:      faq.MainExample,
+		Answers:          answers,
+		IsTrained:        faq.IsTrained,
+		TrainingExamples: trainingExamples,
+	}
+}
--- src/usecases/usecases.go
+++ src/usecases/usecases.go
@@ -1,15 +1,127 @@
 package usecases
 
 import (
-	"github.com/NicolaMassarenti/be-dashboard-bert-faqclass/domain"
+	"fmt"
+
+	"github.com/NicolaMassarenti/be-dashboard-bert-faqclass/src/domain"
 )
+
+// Faq contains the data that define a F.A.Q.
+type Faq struct {
+	ID               string
+	MainExample      string
+	Answers          []Answer
+	IsTrained        bool
+	TrainingExamples []TrainingExample
+}
+
+// TrainingExample contain the training examples of a specific language
+type TrainingExample struct {
+	Language string
+	Examples []string
+}
+
+// Answer contains the answer in a language
+type Answer struct {
+	lang   string
+	answer string
+}
 
 //Logger is the interface that manages the logs
 type Logger interface {
-	Log(message string) error
+	Log(message string)
+	Error(message string)
 }
 
 //KnowledgeBaseInteractor is the object that manages the interactions
 type KnowledgeBaseInteractor struct {
 	FaqRepository domain.FaqRepository
+	Logger        Logger
+}
+
+// KnowledgeBase returns all the knowledge base, all the faqs
+func (kbInteractor *KnowledgeBaseInteractor) KnowledgeBase() (faqs []Faq, err error) {
+	var message string
+
+	kbInteractor.Logger.Log("Retrieving the KB")
+	kb, domainErr := kbInteractor.FaqRepository.KnowledgeBase()
+
+	if domainErr != nil {
+		message = "Error retrieving the KB - %s"
+		err = fmt.Errorf(message, domainErr.Error())
+		kbInteractor.Logger.Log(err.Error())
+		return nil, domainErr
+	}
+
+	faqs = make([]Faq, len(kb))
+
+	// Transforming the KB from domain struct to usecase struct
+	for i, faq := range kb {
+		faqs[i] = faqFromDomainLayer(faq)
+	}
+
+	return faqs, nil
+}
+
+// Faq returns a faq with a given ID
+func (kbInteractor *KnowledgeBaseInteractor) Faq(ID string) (Faq, error) {
+	var message string
+
+	message = fmt.Sprintf("Retrieving Faq with ID: %s", ID)
+	kbInteractor.Logger.Log(message)
+
+	faq, domainErr := kbInteractor.FaqRepository.Faq(ID)
+	if domainErr != nil {
+		message = "Error retrieving the Faq with ID: %s - %s"
+		err := fmt.Errorf(message, ID, domainErr.Error())
+		kbInteractor.Logger.Log(err.Error())
+		return Faq{}, err
+	}
+
+	return faqFromDomainLayer(faq), nil
+}
+
+// ChangeTrainingStatus changes the training status of a FAQ
+func (kbInteractor *KnowledgeBaseInteractor) ChangeTrainingStatus(ID string) error {
+	kbInteractor.Logger.Log(fmt.Sprintf("Chaging training status of Faq with ID: %s", ID))
+
+	if domainErr := kbInteractor.FaqRepository.ChangeTrainingStatus(ID); domainErr != nil {
+		message := "Error changing the training status of Faq with ID: %s"
+		err := fmt.Errorf(message, ID, domainErr.Error())
+		kbInteractor.Logger.Log(err.Error())
+		return err
+	}
+	return nil
+}
+
+// AddFaq adds a faq
+func (kbInteractor *KnowledgeBaseInteractor) AddFaq(faq Faq) error {
+	message := "Adding new faq with id %s"
+	kbInteractor.Logger.Log(fmt.Sprintf(message, faq.ID))
+
+	faqDomain := faqToDomainLayer(faq)
+
+	domainErr := kbInteractor.FaqRepository.AddFaq(faqDomain)
+	if domainErr != nil {
+		message = "Error adding the new faq with id %s"
+		err := fmt.Errorf(message, faq.ID, domainErr.Error())
+		kbInteractor.Logger.Log(err.Error())
+		return err
+	}
+	return nil
+}
+
+// DeleteFaq deletes a faq
+func (kbInteractor *KnowledgeBaseInteractor) DeleteFaq(ID string) error {
+	message := "Deleting faq with id: %s"
+	kbInteractor.Logger.Log(fmt.Sprintf(message, ID))
+
+	domainErr := kbInteractor.FaqRepository.DeleteFaq(ID)
+	if domainErr != nil {
+		message = "Error deleting faq with id %s"
+		err := fmt.Errorf(message, ID, domainErr.Error())
+		kbInteractor.Logger.Log(err.Error())
+		return err
+	}
+	return nil
 }
