57ac766b66f3049fa1ea1a30a74f052cb428c442
NicolaMassarenti <nicola.massarenti@gmail.com>
2020-08-04 15:00:56 +0200

    Added the interfaces for the logger

---
+++ src/interfaces/logger.go
@@ -0,0 +1,37 @@
+package interfaces
+
+// LoggerInterface defines the methods to be implemented by the logger
+type LoggerInterface interface {
+	Fatal(message string)
+	Error(message string)
+	Debug(message string)
+	Info(message string)
+}
+
+// CustomLogger defines my logger
+type CustomLogger struct {
+	logger LoggerInterface
+}
+
+// Logger is the type of my custom logger
+type Logger CustomLogger
+
+// Info logs an "info" event
+func (customLogger *CustomLogger) Info(message string) {
+	customLogger.logger.Info(message)
+}
+
+// Debug logs a "debug" event
+func (customLogger *CustomLogger) Debug(message string) {
+	customLogger.logger.Debug(message)
+}
+
+// Error logs an "error" event
+func (customLogger *CustomLogger) Error(message string) {
+	customLogger.logger.Error(message)
+}
+
+// Fatal logs a "fatal" event
+func (customLogger *CustomLogger) Fatal(message string) {
+	customLogger.logger.Fatal(message)
+}
