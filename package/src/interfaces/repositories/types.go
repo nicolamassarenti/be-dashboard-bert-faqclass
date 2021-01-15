baf55031d367d29454e9b9ade33da7e62dbee36f
nicolamassarenti <nicola.massarenti@gmail.com>
2021-01-15 23:02:13 +0100

    Modified comments

--- package/src/interfaces/repositories/types.go
+++ package/src/interfaces/repositories/types.go
@@ -4,7 +4,7 @@ import (
 	"github.com/nicolamassarenti/be-dashboard-bert-faqclass/src/domain"
 )
 
-// DBHandler is the interface for the handler of the DB for the FAQ
+// DBHandler defines the interface of the db handler
 type DBHandler interface {
 	Add(collection string, faq *map[string]interface{}) error
 	ChangeBool(collection string, ID, path string, value bool) error
@@ -14,40 +14,40 @@ type DBHandler interface {
 	Update(collection string, ID string, data map[string]interface{}) error
 }
 
-// repositoryFaq is the Data retrieved by the repository
+// repositoryFaq defines the Faq managed by the repository
 type repositoryFaq struct {
 	ID   string     `json:"ID,omitempty"`
 	Data domain.Faq `json:"data,omitempty"`
 }
 
-// repositoryKeyword is the Data retrieved by the repository
+// repositoryKeyword defines the Keyword managed by the repository
 type repositoryKeyword struct {
 	ID   string         `json:"ID,omitempty"`
 	Data domain.Keyword `json:"Data,omitempty"`
 }
 
-// LanguageDBRepo is the object for the languages db handler
+// LanguageDBRepo defines the DB handler and the collection for the languages
 type LanguageDBRepo struct {
 	Handler    DBHandler
 	collection string
 }
 
-// FaqDBRepo is the object for the faq db handler
+// FaqDBRepo defines the DB handler and the collection for the faqs
 type FaqDBRepo struct {
 	Handler    DBHandler
 	collection string
 }
 
-// KeywordDBRepo is the object that manages the operations with the keywords
+// KeywordDBRepo defines the DB handler and the collection for the keywords
 type KeywordDBRepo struct {
 	Handler    DBHandler
 	collection string
 }
 
-// KBHandler is the handler for the FAQ
+// KBHandler is the handler for the faqs
 type KBHandler FaqDBRepo
 
-// KeywordHandler is the handler for the Keywords
+// KeywordsHandler is the handler for the keywords
 type KeywordsHandler KeywordDBRepo
 
 // LanguagesHandler is the handler for tha languages
@@ -55,7 +55,6 @@ type LanguagesHandler LanguageDBRepo
 
 // NewLanguagesDBHandler creates a new handler for the languages
 func NewLanguagesDBHandler(dbHandler DBHandler, collection string) *LanguagesHandler {
-
 	languagesHandler := new(LanguagesHandler)
 	languagesHandler.Handler = dbHandler
 	languagesHandler.collection = collection
@@ -64,7 +63,6 @@ func NewLanguagesDBHandler(dbHandler DBHandler, collection string) *LanguagesHan
 
 // NewFaqDBHandler creates a new handler for the faq
 func NewFaqDBHandler(dbHandler DBHandler, collection string) *KBHandler {
-
 	kbHandler := new(KBHandler)
 	kbHandler.Handler = dbHandler
 	kbHandler.collection = collection
@@ -73,7 +71,6 @@ func NewFaqDBHandler(dbHandler DBHandler, collection string) *KBHandler {
 
 // NewKeywordsDBHandler creates a new handler for the keywords
 func NewKeywordsDBHandler(dbHandler DBHandler, collection string) *KeywordsHandler {
-
 	keywordsHandler := new(KeywordsHandler)
 	keywordsHandler.Handler = dbHandler
 	keywordsHandler.collection = collection
