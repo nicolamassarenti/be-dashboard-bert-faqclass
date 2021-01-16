44f23e025cbe3fdf5a50aacff6ac5040130e85c1
nicolamassarenti <nicola.massarenti@gmail.com>
2021-01-16 01:09:10 +0100

    Modified function name, added logs and comment

--- package/src/usecases/kb.go
+++ package/src/usecases/kb.go
@@ -7,10 +7,13 @@ import (
 
 // AddFaq adds a faq
 func (interactor *KnowledgeBaseInteractor) AddFaq(faq Faq) error {
+	interactor.Logger.Debug("Starting to transform data")
+	faqDomain := usecaseFaqToDomainFaq(faq)
+	interactor.Logger.Debug("Data transformed")
 
-	faqDomain := faqToDomainLayer(faq)
-
+	interactor.Logger.Debug("Starting to add faq")
 	domainErr := interactor.Repository.AddFaq(faqDomain)
+	interactor.Logger.Debug("Faq added")
 	if domainErr != nil {
 		message := "error adding a new faq"
 		err := fmt.Errorf(message, domainErr.Error())
@@ -22,30 +25,21 @@ func (interactor *KnowledgeBaseInteractor) AddFaq(faq Faq) error {
 
 // ChangeTrainingStatus changes the training status of a FAQ
 func (interactor *KnowledgeBaseInteractor) ChangeTrainingStatus(ID string, newStatus bool) error {
-
-	if ID == "" {
-		message := "ID not valid - received %s"
-		err := fmt.Errorf(message, ID)
-		interactor.Logger.Error(err.Error())
-		return err
-	}
-
-	interactor.Logger.Info(fmt.Sprintf("Chaging training status of Data with ID: %s", ID))
-
+	interactor.Logger.Debug("Starting to change training status")
 	if domainErr := interactor.Repository.ChangeTrainingStatus(ID, newStatus); domainErr != nil {
 		message := "error changing the training status of Data with ID: %s"
 		err := fmt.Errorf(message, ID, domainErr.Error())
 		interactor.Logger.Error(err.Error())
 		return err
 	}
+	interactor.Logger.Debug("Training status changed")
 	return nil
 }
 
 // DeleteFaq deletes a faq
 func (interactor *KnowledgeBaseInteractor) DeleteFaq(ID string) error {
-	message := "Deleting faq with id: %s"
-	interactor.Logger.Info(fmt.Sprintf(message, ID))
-
+	var message string
+	interactor.Logger.Debug("Starting to delete faq")
 	domainErr := interactor.Repository.DeleteFaq(ID)
 	if domainErr != nil {
 		message = "Error deleting faq with id %s"
@@ -53,16 +47,14 @@ func (interactor *KnowledgeBaseInteractor) DeleteFaq(ID string) error {
 		interactor.Logger.Error(err.Error())
 		return err
 	}
+	interactor.Logger.Debug("Faq deleted")
 	return nil
 }
 
-// Faq returns a faq with a given ID
+// Faq returns a faq
 func (interactor *KnowledgeBaseInteractor) Faq(ID string) (Faq, error) {
 	var message string
-
-	message = fmt.Sprintf("Retrieving Data with ID: %s", ID)
-	interactor.Logger.Info(message)
-
+	interactor.Logger.Debug("Starting to retrieve faq")
 	faq, domainErr := interactor.Repository.Faq(ID)
 	if domainErr != nil {
 		message = "Error retrieving the Data with ID: %s - %s"
@@ -70,19 +62,19 @@ func (interactor *KnowledgeBaseInteractor) Faq(ID string) (Faq, error) {
 		interactor.Logger.Error(err.Error())
 		return Faq{}, err
 	}
-
-	return faqFromDomainLayer(faq), nil
+	interactor.Logger.Debug("Faq retrieved")
+	return domainFaqToUsecaseFaq(faq), nil
 }
 
 // KnowledgeBase returns all the knowledge base, all the faqs
 func (interactor *KnowledgeBaseInteractor) KnowledgeBase() (faqs []Faq, err error) {
 	var message string
 
-	interactor.Logger.Info("Retrieving the KB")
+	interactor.Logger.Debug("Starting to retrieve KB")
 	kb, domainErr := interactor.Repository.KnowledgeBase()
 
 	if domainErr != nil {
-		message = "Error retrieving the KB - %s"
+		message = "Error retrieving KB - %s"
 		err = fmt.Errorf(message, domainErr.Error())
 		interactor.Logger.Error(err.Error())
 		return nil, domainErr
@@ -92,8 +84,9 @@ func (interactor *KnowledgeBaseInteractor) KnowledgeBase() (faqs []Faq, err erro
 
 	// Transforming the KB from domain struct to usecase struct
 	for i, faq := range kb {
-		faqs[i] = faqFromDomainLayer(faq)
+		faqs[i] = domainFaqToUsecaseFaq(faq)
 	}
+	interactor.Logger.Debug("Knowledge base retrieved")
 
 	interactor.Logger.Info("Retrieved " + strconv.Itoa(len(faqs)) + " faqs")
 	return faqs, nil
@@ -101,18 +94,20 @@ func (interactor *KnowledgeBaseInteractor) KnowledgeBase() (faqs []Faq, err erro
 
 // Update updates an existing faq
 func (interactor *KnowledgeBaseInteractor) Update(ID string, faq Faq) error {
-	message := "Updating faq with id: %s"
-	interactor.Logger.Info(fmt.Sprintf(message, ID))
+	interactor.Logger.Debug("Starting to transform data")
+	faqDomain := usecaseFaqToDomainFaq(faq)
+	interactor.Logger.Debug("Data transformed")
 
-	faqDomain := faqToDomainLayer(faq)
-
+	interactor.Logger.Debug("Starting to update faq")
 	domainErr := interactor.Repository.Update(ID, faqDomain)
 	if domainErr != nil {
+		var message string
 		message = "Error deleting faq with id %s"
 		err := fmt.Errorf(message, ID, domainErr.Error())
 		interactor.Logger.Error(err.Error())
 		return err
 	}
+	interactor.Logger.Debug("Faq updated")
 	return nil
 }
 
--- package/src/usecases/utils.go
+++ package/src/usecases/utils.go
@@ -4,8 +4,8 @@ import (
 	"github.com/nicolamassarenti/be-dashboard-bert-faqclass/src/domain"
 )
 
-// faqFromDomainLayer transforms a FAQ from the struct of domain to the struct of the usecases layer
-func faqFromDomainLayer(faq domain.Faq) Faq {
+// domainFaqToUsecaseFaq transforms a FAQ from the struct of domain to the struct of the usecases layer
+func domainFaqToUsecaseFaq(faq domain.Faq) Faq {
 	answers := make([]Answer, len(faq.Answers))
 	for i, ans := range faq.Answers {
 		answers[i] = Answer{ans.Language, ans.Answers}
@@ -18,8 +18,8 @@ func faqFromDomainLayer(faq domain.Faq) Faq {
 	return Faq{faq.ID, faq.MainExample, answers, faq.IsTrained, trainingExamples}
 }
 
-// faqToDomainLayer transforms a FAQ from the struct of usecases to the one of the domain layer
-func faqToDomainLayer(faq Faq) domain.Faq {
+// usecaseFaqToDomainFaq transforms a FAQ from the struct of usecases to the one of the domain layer
+func usecaseFaqToDomainFaq(faq Faq) domain.Faq {
 
 	answers := make([]domain.Answers, len(faq.Answers))
 	for i, ans := range faq.Answers {
